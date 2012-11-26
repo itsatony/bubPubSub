6292a01bc6f8d3f0c09408dc97a55ee791ceb43c
Frank <frank.boeddeker@gmx.net>
2012-11-26 18:20:27 +0100

    wasPublishedOnTopic chainCallSubscriber

--- test/test.js
+++ test/test.js
@@ -19,7 +19,7 @@ describe('BubPubSub', function() {
 	});
 	
 	it("should check the default constructor attributes", function(done){
-		console.log(myBubPubSub);
+		//console.log(myBubPubSub);
 		
 		myBubPubSub.VERSION.should.equal('0.8.7');
 		myBubPubSub.id.should.equal(bubSubId);
@@ -77,7 +77,7 @@ describe('BubPubSub', function() {
  		done();
 	});
 	
-	it("should check publish methode", function(done){
+	it("should check publish method", function(done){
 		var regexForId = /pub_*/;
 		
 		var path = '/politics/europe';
@@ -106,7 +106,113 @@ describe('BubPubSub', function() {
  		myPublisher.settings.bubble.should.be.a('boolean');
  		myPublisher.settings.persist.should.be.a('boolean');
  		myPublisher.publisher.should.equal(publisherName);
- 		console.log(myPublisher);
+ 		//console.log(myPublisher);
  		done();
+	});
+	
+	it("should unsubscribe", function(done){
+		var path = '/politics/europe';
+		var subscriptionName = 'myLittleListener';
+		
+		var mySubscription = myBubPubSub.subscribe(
+ 			path, 
+ 			function(data) { 
+ 				//console.log(data.originalTopic); 
+ 			},  
+ 			{ getBubbles: true, getPersists: false }, 
+ 			subscriptionName
+ 		);
+ 		
+ 		should.exist(myBubPubSub.publicationChannels[path]);
+ 		var unsub = myBubPubSub.unsubscribe(mySubscription);
+ 		
+ 		myBubPubSub.publicationChannels.should.not.be.arguments;
+ 		done();
+	});
+	
+	/**
+	 * just checks if method returns true another test should check if the "fired parameter"
+	 * is getting bigger
+	 */
+	it(" should test if method wasPublishedOnTopic returns true", function(done){
+		var path = '/politics/europe';
+		var publishedPath = '/politics';
+		var content = 'this is wonderfool';
+		var publisherName = 'myTestPublisher';
+		var myPublisher = myBubPubSub.publish(
+ 			path, 
+ 			{ 
+ 				content: content,  
+			},
+			{ 
+				bubble:true, 
+				persist:true 
+			}, 
+			publisherName
+ 		);
+ 		//console.log(myBubPubSub);
+ 		//console.log('##########################################');
+ 		var matchingPubs = myBubPubSub.wasPublishedOnTopic(publishedPath, true);
+ 		//console.log(matchingPubs);
+ 		matchingPubs[0].topic.should.equal(path);
+ 		matchingPubs[0].data.content.should.equal(content);
+ 		matchingPubs[0].data.originalTopic.should.equal(path);
+ 		matchingPubs[0].settings.bubble.should.equal(true);
+ 		matchingPubs[0].settings.persist.should.equal(true);
+ 		matchingPubs[0].publisher.should.equal(publisherName);
+ 		matchingPubs[0].subscribersFired.should.be.empty;
+ 		done();
+	});
+	
+	it(" should test the method chainCallSubscribers", function(done){
+		var publication = { topic: '/politics/europe',
+  							data: 
+   							{ content: 'this is wonderfool',
+     						  originalTopic: '/politics/europe',
+     						  uniquePublicationId: 'pub_1353948822809_88533' },
+  							  settings: { bubble: true, persist: true },
+  							  timestamp: 1353948822809,
+  							  publisher: 'myTestPublisher',
+  							  subscribersFired: [] 
+  						   };
+  						
+  		var config = { scope: 
+   						{ VERSION: '0.8.7',
+    					  id: 'bubpubsubId',
+     					  defaults: 
+      					  { bubble: true,
+        				    persist: false,
+        				    getBubbles: true,
+        				    getPersists: false,
+        				    chain: false,
+        					scope: [],
+        					debugging: 0,
+        					forceUniqueSubscriber: true,
+        					skipOverFailedSubscribers: true,
+        					chainDelay: 0,	
+        					throwErrors: false,
+        					catchSubscriberErrors: false },
+     					errors: [],
+     					publicationChannels: {},
+     					publicationLog: [],
+     					publicationAnnouncements: [] },
+  					persist: true,
+  					bubble: true,
+  					chain: false,
+  					chainDelay: 0,
+  					silent: false };
+
+
+		
+		(myBubPubSub.chainCallSubscribers(publication, config, [])).should.equal(true);
+		done();
+	});
+	
+	/**
+	 * checks the wasPublishedOnTopic with fired parameter from subscriber
+	 */
+	it("should check wasPublishedOnTopic with fired parameter", function(done){
+		//TODO
+		done();
 	});
 });
